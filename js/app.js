c0c644d8a3d490d01bdd59065ac925e9cfb2c233
Avoral <emil.dworakowski@gmail.com>
2017-01-17 22:06:22 +0100

    Click not hover changes

--- js/app.js
+++ js/app.js
@@ -15,19 +15,20 @@ var buttons = document.querySelectorAll('.guzik');
         e.target.querySelector("." + e.target.dataset.button).classList.toggle("rotate");
       }
  });
-
-    //  button.addEventListener("mouseout", function(e){
-    //    e.target.querySelector("." + e.target.dataset.button).classList.remove("rotate");
-    //  });
      }
     else   // If data-button not exist rotate .av-rotate
     {
-      button.addEventListener("mouseover", function(){
-        button.querySelector(".av-rotate").classList.toggle("rotate");
-      });
-      button.addEventListener("mouseout", function(){
-        button.querySelector(".av-rotate").classList.toggle("rotate");
-      });
+      if (button.dataset.button )   // If data exist
+      {
+        button.addEventListener("click", function(e){
+          if (!e.target.querySelector(".av-rotate")) {
+            e.target.classList.toggle("rotate");
+          }
+          else {
+           e.target.querySelector(".av-rotate").classList.toggle("rotate");
+         }
+        });
+
 
      }
  }
